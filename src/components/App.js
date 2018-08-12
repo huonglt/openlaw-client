2a63dd0bb5a5b9d2af7aa19ffcc12679e41e7422
Le Thu Huong <lthuongvn@yahoo.com>
2018-08-12 15:38:56 +1200

    initial setup of the app

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>OpenLaw - File Uploader</h1>
+            </div>
+        );
+    }
+}
