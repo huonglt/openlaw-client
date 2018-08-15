1f0b8f8b776055c8b3d52c9bad26d4af2a0e5172
Le Thu Huong <lthuongvn@yahoo.com>
2018-08-15 22:03:44 +1200

    Add comment

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import store from './store/index';
 
 import AppContainer from './containers/AppContainer';
 
+/**
+ * Provide the store and render the AppContainer component to index.html page
+ */
 ReactDOM.render(
     <Provider store={store}>
       <AppContainer/>
     </Provider>, document.getElementById('root')
-  );
+);
