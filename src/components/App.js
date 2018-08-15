1e3082ed33d90427bf4d37470d77bc4c63b69654
Le Thu Huong <lthuongvn@yahoo.com>
2018-08-15 19:41:17 +1200

    The App component. Display FileUpload component, when submited display parsed result. If parse result contains error, display error message instead

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import FileUploader from './FileUploader';
+import FileUploaderResult from './FileUploaderResult';
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
+        console.log(`this.props = ${JSON.stringify(this.props)}`);
+        const parsedResult = this.props.parsedResult;
+        
         return (
-            <div>
-                <h1>OpenLaw - File Uploader</h1>
+            <div className="container">
+                <div className="row m-3"><h3>Open Law - File Uploader</h3></div>
+                <div className="row"><FileUploader upload={this.props.uploadFile}/></div>
+                {parsedResult && parsedResult.totalWordCount && <div className="row"><FileUploaderResult {...this.props.parsedResult}/></div>}
+                {parsedResult && parsedResult.errMsg && <div>{parsedResult.errMsg}</div>}
             </div>
         );
     }
