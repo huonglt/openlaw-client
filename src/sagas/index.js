90d34c69740ca20d68cb015387095a66abf8cd5b
Le Thu Huong <lthuongvn@yahoo.com>
2018-08-15 21:57:56 +1200

    Add comment for the rootSaga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,6 +2,9 @@ import { takeEvery } from 'redux-saga/effects';
 import { UPLOAD_FILE } from '../actions/index';
 import { uploadFileSaga } from './uploadFile';
 
+/**
+ * The root saga of the app
+ */
 export function* rootSaga() {
   yield takeEvery(UPLOAD_FILE, uploadFileSaga);
 }
