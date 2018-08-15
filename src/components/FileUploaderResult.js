bcde656d4995ca602399a5577a216ab58b8707ac
Le Thu Huong <lthuongvn@yahoo.com>
2018-08-15 21:56:04 +1200

    Add comment for FileUploaderResult

--- src/components/FileUploaderResult.js
+++ src/components/FileUploaderResult.js
@@ -1,4 +1,8 @@
 import React from 'react';
+
+/**
+ * This component display a table with total word count and the count of each word occurence
+ */
 export default class FileUploaderResult extends React.Component {
     constructor(props) {
         super(props);
@@ -8,8 +12,7 @@ export default class FileUploaderResult extends React.Component {
     wordOccurenceBlock(wordOccurences) {
         return Object.keys(wordOccurences).map((word, index) => {
             return (
-                <li className="list-group-item d-flex justify-content-between align-items-center" key={index}>
-                    {word}
+                <li className="list-group-item d-flex justify-content-between align-items-center" key={index}>{word}
                     <span className="badge badge-primary badge-pill">{wordOccurences[word]}</span>
                 </li>
             );
@@ -20,7 +23,6 @@ export default class FileUploaderResult extends React.Component {
         const { totalWordCount, wordOccurences } = this.props;
         return (
             <div className="container mt-4">
-                
                 <ul className="list-group">
                     <li className="list-group-item d-flex justify-content-between align-items-center list-group-item-primary">
                     Total Word Count
@@ -30,8 +32,7 @@ export default class FileUploaderResult extends React.Component {
                 <ul className="list-group">
                     <li className="list-group-item d-flex justify-content-between align-items-center list-group-item-primary">Word Occurrences</li>
                     {this.wordOccurenceBlock(wordOccurences)}
-                </ul>    
-                
+                </ul>
             </div>
         );
     }
