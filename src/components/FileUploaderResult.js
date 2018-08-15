1ba779926a87061f91b7c273e9991d52d6dbf833
Le Thu Huong <lthuongvn@yahoo.com>
2018-08-15 19:38:46 +1200

    The component to display the parsing result returned from the server

---
+++ src/components/FileUploaderResult.js
@@ -0,0 +1,38 @@
+import React from 'react';
+export default class FileUploaderResult extends React.Component {
+    constructor(props) {
+        super(props);
+        this.wordOccurenceBlock = this.wordOccurenceBlock.bind(this);
+    }
+
+    wordOccurenceBlock(wordOccurences) {
+        return Object.keys(wordOccurences).map((word, index) => {
+            return (
+                <li className="list-group-item d-flex justify-content-between align-items-center" key={index}>
+                    {word}
+                    <span className="badge badge-primary badge-pill">{wordOccurences[word]}</span>
+                </li>
+            );
+        });
+    }
+
+    render() {
+        const { totalWordCount, wordOccurences } = this.props;
+        return (
+            <div className="container mt-4">
+                
+                <ul className="list-group">
+                    <li className="list-group-item d-flex justify-content-between align-items-center list-group-item-primary">
+                    Total Word Count
+                    <span className="badge badge-primary badge-pill">{totalWordCount}</span>
+                </li>
+                </ul>
+                <ul className="list-group">
+                    <li className="list-group-item d-flex justify-content-between align-items-center list-group-item-primary">Word Occurrences</li>
+                    {this.wordOccurenceBlock(wordOccurences)}
+                </ul>    
+                
+            </div>
+        );
+    }
+}
