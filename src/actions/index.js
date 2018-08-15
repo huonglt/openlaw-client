c26b52a35bfe4aa3db1185130b70487fa1e59ceb
Le Thu Huong <lthuongvn@yahoo.com>
2018-08-15 21:50:25 +1200

    Add comment for actions.js module

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,6 @@
+/**
+ * A module containing actions and action functions
+ */
 export const UPLOAD_FILE = 'UPLOAD_FILE';
 export const UPLOAD_FILE_SUCCESS = 'UPLOAD_FILE_SUCCESS';
 export const UPLOAD_FILE_FAILED = 'UPLOAD_FILE_FAILED';
