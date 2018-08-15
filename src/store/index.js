f4f4835edb3fcb78c3d690bc7be064387cfa2833
Le Thu Huong <lthuongvn@yahoo.com>
2018-08-15 22:02:22 +1200

    Add comment for setting up the store, and sagas

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,7 @@ import { rootSaga } from '../sagas/index';
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware();
 
+// create the redux store with the sagaMiddleware
 const store = createStore(reducers, {}, applyMiddleware(sagaMiddleware));
 
 // then run the saga
