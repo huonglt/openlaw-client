ea7608dc2b3e8390454c06b3dee69f239aa078c4
Le Thu Huong <lthuongvn@yahoo.com>
2018-08-15 22:01:41 +1200

    Add comment for the reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,5 +1,11 @@
 import { UPLOAD_FILE_SUCCESS, UPLOAD_FILE_FAILED } from '../../actions/index';
 
+/**
+ * The reducer of the store
+ * Handle UPLOAD_FILE_SUCCESS and UPLOAD_FILE_FAILED actions dispatched by the uploadFile saga
+ * When UPLOAD_FILE_SUCCESS action, it sets the parsedResult to the state, and reset the err to null
+ * When UPLOAD_FILE_FAILED action, it sets the parsedResult to null and sets the err to a friendly message
+ */
 const reducers = (state, action) => {
     switch(action.type) {
         case UPLOAD_FILE_SUCCESS:
