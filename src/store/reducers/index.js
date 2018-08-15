7018bac4f9a4a7a099a48eb812029767434dc159
Le Thu Huong <lthuongvn@yahoo.com>
2018-08-15 20:09:16 +1200

    When server returns error, display a friendly message to the user

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,9 +3,9 @@ import { UPLOAD_FILE_SUCCESS, UPLOAD_FILE_FAILED } from '../../actions/index';
 const reducers = (state, action) => {
     switch(action.type) {
         case UPLOAD_FILE_SUCCESS:
-            return { ...state, parsedResult: action.parsedResult };
+            return { ...state, parsedResult: action.parsedResult, err: null };
         case UPLOAD_FILE_FAILED:
-            return { ...state, parsedResult: null };    
+            return { ...state, parsedResult: null, err: 'Sorry something wrong. Check your file is an ascii file or server is running' };    
     }
     return state;
 }
