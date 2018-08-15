c0dce54f9ee9b578aae8e5db41f5fed4f40b82be
Le Thu Huong <lthuongvn@yahoo.com>
2018-08-15 22:17:24 +1200

    Rename FileUploaderResutl to FielParsedResult

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import FileUploader from './FileUploader';
-import FileUploaderResult from './FileUploaderResult';
+import FileParsedResult from './FileParsedResult';
 
 /**
  * A React component representing the App
@@ -17,7 +17,7 @@ export default class App extends Component {
                 <div className="row m-3"><h3>Open Law - ASCII File Parsing</h3></div>
                 <div className="row m-3"><h6>Upload your ASCII file for parsing - No larger than 10MB</h6></div>
                 <div className="row"><FileUploader upload={this.props.uploadFile}/></div>
-                {parsedResult && parsedResult.totalWordCount && <div className="row"><FileUploaderResult {...this.props.parsedResult}/></div>}
+                {parsedResult && parsedResult.totalWordCount && <div className="row"><FileParsedResult {...this.props.parsedResult}/></div>}
                 {parsedResult && parsedResult.errMsg && <div>{parsedResult.errMsg}</div>}
                 {err && <div>{err}</div>}
             </div>
