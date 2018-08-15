62204223096d1099b957c8280557a7838736891c
Le Thu Huong <lthuongvn@yahoo.com>
2018-08-15 20:14:59 +1200

    Add more descriptive text to the page

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,8 @@ export default class App extends Component {
         
         return (
             <div className="container">
-                <div className="row m-3"><h3>Open Law - File Uploader</h3></div>
+                <div className="row m-3"><h3>Open Law - ASCII File Parsing</h3></div>
+                <div className="row m-3"><h6>Upload your ASCII file for parsing - No larger than 10MB</h6></div>
                 <div className="row"><FileUploader upload={this.props.uploadFile}/></div>
                 {parsedResult && parsedResult.totalWordCount && <div className="row"><FileUploaderResult {...this.props.parsedResult}/></div>}
                 {parsedResult && parsedResult.errMsg && <div>{parsedResult.errMsg}</div>}
